64171b69b67f43b4dcdab56be2426eaaf7672734
Lazar Eric <lazareric.com@gmail.com>
2023-05-06 20:32:19 +0200

    Build docs types

--- utils/js/build.js
+++ utils/js/build.js
@@ -267,9 +267,13 @@ async function docsUpdateTypes(pathTypes, pathUse, isModules) {
 
   const use = fse.existsSync(usePath) ? await fse.readFile(usePath, 'utf8') : '';
 
-  let values = use?.trim().match(/(?:^|}~)[^~]+(?:$|~{)/ig) || [];
+  let values = use?.trim().match(/(?!^|}~)[^~]+(?!$|~{)/ig) || [];
 
-  const parts = data.match(/((type|const) [^{|\n]+{\n[^}]+};)|((type|const|function) [^\n]+)|(interface [^}]+};?\n)/ig) || [];
+  if (values[0]?.startsWith('#')) values[0] = `#${values[0]}`;
+
+  if (values[values.length - 1]?.endsWith('\n``')) values[values.length - 1] = values[values.length - 1] + '`';
+
+  const parts = data.match(/((type|const) [^{|\n]+{\n[^}]+};)|((type|const|function) [^\n]+)|((interface|class) [^}]+};?\n)/ig) || [];
 
   let valueNew = `\n\n### API\n\n`;
 
